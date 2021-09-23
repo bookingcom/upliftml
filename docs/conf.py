d80ea94fd5af532b311db4f37e7cb5d647e05878
Irene Teinemaa <irene.teinemaa@gmail.com>
2021-09-23 17:21:38 +0200

    Added autodoc mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,8 @@ release = '0.0.1'
 extensions = ['sphinxcontrib.bibtex', 'sphinx.ext.autodoc']
 bibtex_bibfiles = ['refs.bib']
 
+autodoc_mock_imports = ['pyspark', 'h2o', 'matplotlib', 'numpy']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
