2e5b2ed4dc7189b7ca744a4f8138d022b7e1829e
Jannik <roessler.jannik@gmx.de>
2022-11-15 09:23:06 +0100

    Bugfix in assert

--- upliftml/models/h2o.py
+++ upliftml/models/h2o.py
@@ -873,7 +873,7 @@ class UpliftRandomForestEstimator:
             df_h2o_val (h2o.H2OFrame, optional): a dataframe containing the treatment indicators, the observed outcomes, and predictors
         """
         
-        assert df_h2o_train[self.target_colname].isfactor()[0] == True, 'The target column must be categorical as the estimator only supports binomial classification. '
+        assert df_h2o_train[self.target_colname].isfactor()[0] is True, 'The target column must be categorical as the estimator only supports binomial classification.'
         
         factor_cols = [self.treatment_colname]
         if self.categorical_outcome:
