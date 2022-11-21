20efd5654e4d93c745e2c01adad8ecc7241c2e1d
Jannik <roessler.jannik@gmx.de>
2022-11-21 09:35:13 +0100

    Added assert for binary values only

--- upliftml/models/h2o.py
+++ upliftml/models/h2o.py
@@ -873,6 +873,10 @@ class UpliftRandomForestEstimator:
             df_h2o_val (h2o.H2OFrame, optional): a dataframe containing the treatment indicators, the observed outcomes, and predictors
         """
 
+        assert (
+            df_h2o_train[self.target_colname].as_data_frame()[self.target_colname].nunique() > 2
+        ), "The target column must contain binary values as the estimator only supports binomial classification."
+
         factor_cols = [self.treatment_colname]
         if self.categorical_outcome:
             factor_cols.append(self.target_colname)
